a6ac558b46b075f132b22e6bbb2e7dc1478504b6
Nestle <nestle.mhcp0001@gmail.com>
2023-12-19 02:50:28 +0900

    setting ignore

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import os
+from notion_client import Client
+
+token = os.environ.get("NOTION_TOKEN")
+client = Client(auth=token)  # token: インテグレーションのシークレット情報
+
+
+def get_all_notion_users():
+    response = client.users.list()
+    return response
