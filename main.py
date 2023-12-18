4dd30b28749c609642e0b1bf68b61840f12e1b0e
Nestle <nestle.mhcp0001@gmail.com>
2023-12-19 03:16:55 +0900

    Notion APIでページを追加

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import os
 from notion_client import Client
+from settings import *
 
-token = os.environ.get("NOTION_TOKEN")
+
+token = NOTION_API_TOKEN
+database_id = NOTION_DATABASE_ID
 client = Client(auth=token)  # token: インテグレーションのシークレット情報
 
 
-def get_all_notion_users():
-    response = client.users.list()
-    return response
+def add_row_to_notion_database(database_id):
+    response = client.pages.create(
+        **{
+            "parent": {"database_id": database_id},
+            # ページのプロパティを指定する
+            "properties": {
+                "名前": {"title": [{"text": {"content": "Pythonで追加"}}]},
+                "Status": {
+                    "status": {
+                        "name": "未着手",  # Done, In progress, Not started
+                    }
+                },
+                "気持ち": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": f"test",
+                            }
+                        }
+                    ]
+                },
+            },  # end properties
+        }
+    )
+
+    print("notion database create completed")
+    print(response)  # 追加した内容を表示する
+
+
+print(add_row_to_notion_database(database_id))
